d5eeb132939ea368616f053c4d151c8ba91238aa
brajasekharreddy <raj479.mits@gmail.com>
2023-02-01 23:07:53 +0530

    Script to Update jenkins server public IP with DNS

---
+++ update-dns-record.sh
@@ -0,0 +1,21 @@
+IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=jenkins"  --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
+
+echo '
+{
+  "Comment": "CREATE/DELETE/UPSERT a record ",
+  "Changes": [{
+    "Action": "UPSERT",
+    "ResourceRecordSet": {
+      "Name": "jenkins.devopsb70.online",
+      "Type": "A",
+      "TTL": 15,
+      "ResourceRecords": [{ "Value": "IPADDRESS"}]
+    }}]
+}' | sed -e "s/IPADDRESS/${IP}/" >/tmp/jenkins.json
+
+ZONE_ID="Z0366464237Z7LZLZPKFA"
+aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/jenkins.json | jq .
+
+
+
+
